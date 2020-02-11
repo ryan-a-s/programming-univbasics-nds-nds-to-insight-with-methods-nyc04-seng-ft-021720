cb137ae60caa32b0b159929337828246403715df
Ryan Seit <ryan@ryanseit.com>
2020-02-11 15:48:25 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -27,7 +27,7 @@ def directors_totals(nds)
   result = {}
   index = 0
   while index < nds.length do
-    result[nds][index] = gross_for_director(nds[index])
+    result[nds][index] = gross_for_director(nds[index][:name])
     index += 1
   end
   result
