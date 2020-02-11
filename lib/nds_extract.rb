8bfbd12d871423855203a2e214dfe4180094a669
Ryan Seit <ryan@ryanseit.com>
2020-02-11 19:15:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,7 +9,7 @@ def gross_for_director(director_data)
   while index < director_data.length do
     total_gross = 0
     inner_index = 0
-    if director_data.find{ |x| x[:name] == first_director_name } do
+    if director_data.find{ |x| x[:name] == first_director_name }
       inner_len = director_data[:movies].length
         while inner_index < inner_len do
           total_gross += director_data[:movies][inner_index][:worldwide_gross]
