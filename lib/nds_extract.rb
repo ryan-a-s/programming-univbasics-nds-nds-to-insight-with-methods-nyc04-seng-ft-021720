c86ef865f9b4bc0e84813178ec31a73e5d6f55d4
Ryan Seit <ryan@ryanseit.com>
2020-02-11 15:49:22 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -27,7 +27,7 @@ def directors_totals(nds)
   result = {}
   index = 0
   while index < nds.length do
-    result[nds][index] = gross_for_director(nds[index][:name][:movies])
+    result[nds][index] = gross_for_director(nds[index][:movies])
     index += 1
   end
   result
