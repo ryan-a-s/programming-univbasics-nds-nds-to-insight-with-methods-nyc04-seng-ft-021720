9f26a4fe88403592034470cdaeb61fffda52737a
Ryan Seit <ryan@ryanseit.com>
2020-02-11 16:05:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,6 +4,7 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  pp director_data
   index = 0
   length = director_data[:name][index][:movies].length
   total_gross = 0
